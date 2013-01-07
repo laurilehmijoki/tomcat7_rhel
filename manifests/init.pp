bfdf20b381b8295f3057ee8d56cde30b28188bf2
Tomi Takussaari <tomi.takussaari@elisa.fi>
2013-01-07 17:13:04 +0200

    Depend on latest version of java 7

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,10 @@ class tomcat7_rhel {
   include tomcat7_rhel::jpackage-repo
 
   package { "java-1.7.0-openjdk":
-    ensure => installed
+    ensure => latest
   }
   package { "java-1.7.0-openjdk-devel":
-    ensure => installed,
+    ensure => latest,
     require => Package["java-1.7.0-openjdk"]
   }
 
