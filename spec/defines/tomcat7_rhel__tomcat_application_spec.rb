abf169dabcf00719c4b86bbe5c938b5756fd135b
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-01-21 19:53:29 +0200

    Add more specs for tomcat_application

--- spec/defines/tomcat7_rhel__tomcat_application_spec.rb
+++ spec/defines/tomcat7_rhel__tomcat_application_spec.rb
@@ -1,15 +1,41 @@
 require 'spec_helper'
 
-describe 'tomcat7_rhel::tomcat_application with minimal parameters' do
-  let(:title) { 'my-web-app' }
-  let(:params) {{
-    :application_root => '/opt',
-    :tomcat_user      => 'uzer',
-    :tomcat_port      => 8080,
-    :jvm_envs         => '-Di_love_java=true',
-  }}
+describe 'tomcat7_rhel::tomcat_application' do
+  context 'minimal configuration' do
+    let(:title) { 'my-web-app' }
 
-  it {
-    should include_class('tomcat7_rhel')
-  }
+    let(:params) {{
+      :application_root => '/opt',
+      :tomcat_user      => 'uzer',
+      :tomcat_port      => 8123,
+      :jvm_envs         => '-Di_love_java=true'
+    }}
+
+    it { should include_class('tomcat7_rhel') }
+
+    it {
+      should contain_file('/opt/my-web-app/conf/server.xml').
+        with_content(/.*port="8123".*/m)
+    }
+
+    it {
+      should contain_file('/etc/init.d/my-web-app').with({
+        'ensure' => 'link',
+        'target' => '/etc/init.d/tomcat7'
+      })
+    }
+
+    it {
+      should contain_file('/opt/my-web-app/conf/web.xml').with({
+        'ensure' => 'link',
+        'target' => '/usr/share/tomcat7/conf/web.xml'
+      })
+    }
+
+    it {
+      should contain_service('my-web-app').with({
+        'ensure' => 'running'
+      })
+    }
+  end
 end
