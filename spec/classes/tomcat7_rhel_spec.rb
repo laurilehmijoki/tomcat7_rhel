f0585566a09693543225bbe1d640992636289340
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-09-05 04:59:14 +0300

    Add examples of managing java outside this module

--- spec/classes/tomcat7_rhel_spec.rb
+++ spec/classes/tomcat7_rhel_spec.rb
@@ -1,17 +1,37 @@
 require 'spec_helper'
 
 describe 'tomcat7_rhel' do
-  let(:title) { 'foo' }
+  shared_examples 'default packages' do
+    it {
+      should contain_package('tomcat7').with({
+        'ensure'  => 'installed'
+      })
+    }
 
-  it { should include_class('tomcat7_rhel::jpackage_repo') }
+    it { should include_class('tomcat7_rhel::jpackage_repo') }
+  end
 
-  it {
-    should contain_package('tomcat7').with({
-      'ensure'  => 'installed'
-    })
-  }
+  describe '$manage_java = [default]' do
+    let(:title) { 'foo' }
 
-  it { should contain_package("java-1.7.0-openjdk") }
+    include_examples 'default packages'
 
-  it { should contain_package("java-1.7.0-openjdk-devel") }
+    it { should contain_package("java-1.7.0-openjdk") }
+
+    it { should contain_package("java-1.7.0-openjdk-devel") }
+  end
+
+  describe '$manage_java = false' do
+    let(:title) { 'foo' }
+
+    let(:params) {{
+      :manage_java => false
+    }}
+
+    include_examples 'default packages'
+
+    it { should_not contain_package("java-1.7.0-openjdk") }
+
+    it { should_not contain_package("java-1.7.0-openjdk-devel") }
+  end
 end
